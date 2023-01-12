8c50f7e0d4786cd768e3ecc19c3ef759ca6d0148
kirill-rogachev <kirill-rogachev@yandex.ru>
2023-01-12 22:28:36 +0300

    add Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+
+        String gender = "Male";
+        String name = "Name";
+        int height = 180;
+        int weight = 80;
+        int age = 18;
+
+        void walk() {
+            System.out.println("Человек идет");
+        }
+        void sit() {
+            System.out.println("Человек сидит");
+        }
+        void run() {
+            System.out.println("Человек бежит");
+        }
+        void talk() {
+            System.out.println("Человек говорит");
+        }
+        String learnJava() {
+            return "Человек учит Java";
+        }
+}
