9fe0df36063b6c20daaa64d9e8b1ad6c5d1771e1
kirill-rogachev <kirill-rogachev@yandex.ru>
2023-01-15 21:49:04 +0300

    update WolfTest

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,23 +1,23 @@
 public class WolfTest {
-    public static void main(String args[]) {
-    Wolf wolf = new Wolf();
+    public static void main(String[] args) {
+        Wolf wolf = new Wolf();
 
-    wolf.gender = "самец";
-    wolf.name = "волк";
-    wolf.weight = 20;
-    wolf.age = 5;
-    wolf.color = "серый";
+        wolf.gender = "самец";
+        wolf.name = "волк";
+        wolf.weight = 20;
+        wolf.age = 5;
+        wolf.color = "серый";
 
-    System.out.println(wolf.gender);
-    System.out.println(wolf.name);
-    System.out.println(wolf.weight);
-    System.out.println(wolf.age);
-    System.out.println(wolf.color);
+        System.out.println(wolf.gender);
+        System.out.println(wolf.name);
+        System.out.println(wolf.weight);
+        System.out.println(wolf.age);
+        System.out.println(wolf.color);
 
-    wolf.walk();
-    wolf.sit();
-    wolf.run();
-    wolf.howl();
-    wolf.hunt();
+        wolf.walk();
+        wolf.sit();
+        wolf.run();
+        wolf.howl();
+        wolf.hunt();
     }
 }
