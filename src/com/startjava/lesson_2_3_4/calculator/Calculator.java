f653f7d2feaaed71a1655ccd2307ccc1d5d1e381
kirill-rogachev <kirill-rogachev@yandex.ru>
2023-02-19 21:41:59 +0300

    update calculator

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,53 +2,30 @@ package src.com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    private int a;
-    private int b;
-    private char sign;
-    private String userInput;
-
-    public void setA(int a) {
-        this.a = a;
-    }
-
-    public void setB(int b) {
-        this.b = b;
-    }
-    
-    public void setSign(char sign) {
-        this.sign = sign;
-    }
-
-    public void setUserInput(String userInput) {
-        this.userInput = userInput;
-    }
-
-    public double calculate() {
-        String[] inputItems = userInput.split(" ");
-        a = Integer.parseInt(inputItems[0]);
-        sign = inputItems[1].charAt(0);
-        b = Integer.parseInt(inputItems[2]);
-        double result = 0;
-        switch(sign) {
-            case '+' : 
-                result = Math.addExact(a, b);
-                break;
-            case '-' :
-                result = Math.subtractExact(a, b);
-                break;
-            case '*' :
-                result = Math.multiplyExact(a, b);
-                break;
-            case '/' :
-                result = (double) a / b;
-                break;
-            case '%' :
-                result = Math.floorMod(a, b);
-                break;
-            case '^' :
-                result = Math.pow(a, b);
-                break;
+    public static double calculate(String userInput) {
+            String[] inputItems = userInput.split(" ");
+            try {
+            checkNumbers(Double.parseDouble(inputItems[0]), Double.parseDouble(inputItems[2]));
+            int a = Integer.parseInt(inputItems[0]);
+            char sign = inputItems[1].charAt(0);
+            int b = Integer.parseInt(inputItems[2]);
+            return switch(sign) {
+                case '+' -> Math.addExact(a, b);
+                case '-' -> Math.subtractExact(a, b);
+                case '*' -> Math.multiplyExact(a, b);
+                case '/' -> (double) a / b;
+                case '%' -> Math.floorMod(a, b);
+                case '^' -> Math.pow(a, b);
+                default -> 0;
+            };
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Введите корректные значения");
+            return 0;
         }
-        return result;
+    }
+    public static void checkNumbers(double a, double b) {
+        if((a % 1 != 0) || (b % 1 != 0) || (a <= 0) || (b <= 0)) {
+            System.out.println("Можно вводить только целые положительные числа");
+        }
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,21 +1,16 @@
 package src.com.startjava.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 public class CalculatorTest {
     
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         String userAnswer = "yes";
-        DecimalFormat format = new DecimalFormat();
-        format.setDecimalSeparatorAlwaysShown(false);
         while(!userAnswer.equals("no")) {
             if(userAnswer.equals("yes")) {
                 System.out.print("Введите математическое выражение: ");
-                calculator.setUserInput(scanner.nextLine());
-                System.out.println("Результат = " + format.format(calculator.calculate()));
+                System.out.format("Результат = %f\n", Calculator.calculate(scanner.nextLine()));
             }
             System.out.println("Хотите продолжить вычисления? [yes/no]:");
             userAnswer = scanner.nextLine();
