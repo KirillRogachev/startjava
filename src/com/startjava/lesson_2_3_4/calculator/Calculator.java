429b84451a8988bc2a7c97e26c43c1bd8c23f14a
kirill-rogachev <kirill-rogachev@yandex.ru>
2023-02-19 21:45:12 +0300

    update calculator

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -3,8 +3,8 @@ package src.com.startjava.lesson_2_3_4.calculator;
 public class Calculator {
 
     public static double calculate(String userInput) {
-            String[] inputItems = userInput.split(" ");
-            try {
+        String[] inputItems = userInput.split(" ");
+        try {
             checkNumbers(Double.parseDouble(inputItems[0]), Double.parseDouble(inputItems[2]));
             int a = Integer.parseInt(inputItems[0]);
             char sign = inputItems[1].charAt(0);
@@ -23,6 +23,7 @@ public class Calculator {
             return 0;
         }
     }
+
     public static void checkNumbers(double a, double b) {
         if((a % 1 != 0) || (b % 1 != 0) || (a <= 0) || (b <= 0)) {
             System.out.println("Можно вводить только целые положительные числа");
