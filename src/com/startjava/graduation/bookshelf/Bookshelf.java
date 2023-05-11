daaec5242b4b61f30db253829690011be74d3694
Kirill <kirill-rogachev@yandex.ru>
2023-05-11 17:08:58 +0300

    update Bookshelf

--- src/com/startjava/graduation/bookshelf/Bookshelf.java
+++ src/com/startjava/graduation/bookshelf/Bookshelf.java
@@ -2,49 +2,55 @@ package src.com.startjava.graduation.bookshelf;
 
 public class Bookshelf {
 
-    private static final int SHELF_NUM = 10;
-    private Book[] books = new Book[SHELF_NUM];
-    private int firstEmptyShelf;
+    private static final int SHELVES_NUM = 10;
+    private Book[] books = new Book[SHELVES_NUM];
+    private int booksNum;
     private int maxRecordLen;
 
-    public int getFirstEmptyShelf() { return firstEmptyShelf; }
+    public int getBooksNum() {return booksNum;}
+
+    public int getEmptyShelvesNum() {return (SHELVES_NUM - booksNum);}
 
     public void addBook(String author, String name, int publishYear) {
-        books[firstEmptyShelf] = new Book(author, name, publishYear);
-        if(books[firstEmptyShelf].getRecordLen() > maxRecordLen) {
-            maxRecordLen = books[firstEmptyShelf].getRecordLen();
+        try {
+            books[booksNum] = new Book(author, name, publishYear);
+            if (books[booksNum].getRecordLen() > maxRecordLen) {
+                maxRecordLen = books[booksNum].getRecordLen();
+            }
+            booksNum++;
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Книга не может быть сохранена. Шкаф полностью заполнен.");
         }
-        firstEmptyShelf++;
     }
 
     public String findBook(String titleToFind) {
-        try {
-            for (int i = 0; i < firstEmptyShelf; i++) {
+            for (int i = 0; i < booksNum; i++) {
                 if (books[i].getTitle().equals(titleToFind)) {
                     return books[i].toString();
                 }
-
             }
-        } catch(NumberFormatException e) {
-            System.out.println("Книга не найдена");
-        }
-        return null;
+            return "Книга не найдена";
     }
 
-    public void deleteBook (String title) {
+    public void deleteBook (String searchedTitle) {
         int deletedRecordLen = 0;
-        for(int i = 0; i < firstEmptyShelf; i++) {
-            if(books[i].getTitle().equals(title)) {
+        boolean bookFound = false;
+        for(int i = 0; i < booksNum; i++) {
+            if(books[i].getTitle().equals(searchedTitle)) {
                 deletedRecordLen = books[i].getRecordLen();
                 books[i] = null;
-                System.arraycopy(books, i + 1, books, i, firstEmptyShelf - i - 1);
-                books[--firstEmptyShelf] = null;
+                System.arraycopy(books, i + 1, books, i, booksNum - i - 1);
+                books[--booksNum] = null;
+                bookFound = true;
                 break;
             }
         }
+        if(!bookFound) {
+            System.out.println("Книга не найдена");
+        }
         if(deletedRecordLen == maxRecordLen) {
             maxRecordLen = 0;
-            for(int i = 0; i < firstEmptyShelf; i++) {
+            for(int i = 0; i < booksNum; i++) {
                 if(books[i].getRecordLen() > maxRecordLen) {
                     maxRecordLen = books[i].getRecordLen();
                 }
@@ -53,21 +59,20 @@ public class Bookshelf {
     }
 
     public void clear() {
-        for(int i = 0; i < firstEmptyShelf; i++) {
+        for(int i = 0; i < booksNum; i++) {
             books[i] = null;
         }
-        firstEmptyShelf = 0;
+        booksNum = 0;
         maxRecordLen = 0;
     }
 
     public void showContent() {
-        System.out.println("\nВ шкафу " + firstEmptyShelf + " книг(и) и свободно " + (SHELF_NUM - firstEmptyShelf) +
-            " полок.\n");
-        for(int i = 0; i < firstEmptyShelf; i++) {
+        System.out.println();
+        for(int i = 0; i < booksNum; i++) {
             System.out.printf("|%-" + maxRecordLen + "s|\n", books[i]);
             System.out.println("|" + "-".repeat(maxRecordLen) + "|");
         }
-        if(firstEmptyShelf < SHELF_NUM) {
+        if(booksNum < SHELVES_NUM) {
             System.out.println("|" + " ".repeat(maxRecordLen) + "|");
         }
     }
--- src/com/startjava/graduation/bookshelf/BookshelfTest.java
+++ src/com/startjava/graduation/bookshelf/BookshelfTest.java
@@ -11,48 +11,61 @@ public class BookshelfTest {
         System.out.println(emptyBookshelf);
         boolean continueWork = true;
         while(continueWork) {
+            System.out.println("\nВ шкафу " + bookshelf.getBooksNum() + " книг(и) и свободно " +
+                    bookshelf.getEmptyShelvesNum() + " полок.\n");
             System.out.println("""
+                    Введите номер действия из меню:
                     1. Добавить книгу
                     2. Найти книгу
                     3. Удалить книгу
                     4. Очистить шкаф
                     5. Завершить
                     """);
-            int selectedAction = Integer.parseInt(scanner.nextLine());
-            switch (selectedAction) {
-                case 1:
-                    System.out.print("Введите автора книги: ");
-                    String author = scanner.nextLine();
-                    System.out.print("Введите название книги: ");
-                    String title = scanner.nextLine();
-                    System.out.print("Введите год издания: ");
-                    int publishYear = Integer.parseInt(scanner.nextLine());
-                    bookshelf.addBook(author, title, publishYear);
-                    bookshelf.showContent();
-                    break;
-                case 2:
-                    System.out.print("Введите название книги, которую нужно найти: ");
-                    System.out.println(bookshelf.findBook(scanner.nextLine()));
-                    bookshelf.showContent();
-                    break;
-                case 3:
-                    System.out.print("Введите название книги, которую нужно удалить: ");
-                    bookshelf.deleteBook(scanner.nextLine());
-                    bookshelf.showContent();
-                    break;
-                case 4:
-                    bookshelf.clear();
-                    System.out.println(emptyBookshelf);
-                    break;
-                case 5:
-                    continueWork = false;
-                    break;
-                default:
-                    System.out.println("Введено недопустимое значение. Введите номер операции из меню.");
-                    break;
+
+            try{
+                int selectedAction = Integer.parseInt(scanner.nextLine());
+                switch (selectedAction) {
+                    case 1:
+                        System.out.print("Введите автора книги: ");
+                        String author = scanner.nextLine();
+                        System.out.print("Введите название книги: ");
+                        String title = scanner.nextLine();
+                        System.out.print("Введите год издания: ");
+                        while(true) {
+                            try {
+                                int publishYear = Integer.parseInt(scanner.nextLine());
+                                bookshelf.addBook(author, title, publishYear);
+                                break;
+                            } catch (NumberFormatException e) {
+                                System.out.println("Год должен быть введен в числовом формате");
+                            }
+                        }
+                        break;
+                    case 2:
+                        System.out.print("Введите название книги, которую нужно найти: ");
+                        System.out.println(bookshelf.findBook(scanner.nextLine()));
+                        break;
+                    case 3:
+                        System.out.print("Введите название книги, которую нужно удалить: ");
+                        bookshelf.deleteBook(scanner.nextLine());
+                        break;
+                    case 4:
+                        bookshelf.clear();
+                        System.out.println(emptyBookshelf);
+                        break;
+                    case 5:
+                        continueWork = false;
+                        break;
+                    default:
+                        System.out.println("Введите значение от 1 до 5");
+                        break;
+                }
+                System.out.print("\nДля продолжения работы нажмите Enter");
+                scanner.nextLine();
+                bookshelf.showContent();
+            } catch(NumberFormatException e) {
+                System.out.println("Введено недопустимое значение");
             }
-            System.out.print("Для продолжения работы нажмите Enter");
-            scanner.nextLine();
         }
     }
 }
